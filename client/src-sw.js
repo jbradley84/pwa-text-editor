c056d06e62b677be67d0dc44fad9a64d47f60505
jbradley84 <utbradley84@yahoo.com>
2022-09-09 16:19:42 -0500

    updated asset caching in src-sw.js and event handlers in install.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -26,6 +26,15 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
+// asset caching
 registerRoute(
+   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+   new StaleWhileRevalidate({
+      cacheName: 'assest-cache',
+      plugins: [
+         new CacheableResponsePlugin({
+            statuses: [0, 200],
+         }),
+      ],
+   })
 );
