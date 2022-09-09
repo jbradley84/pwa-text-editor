191613fa846ffe046acc6ced7597219875c19901
jbradley84 <utbradley84@yahoo.com>
2022-09-09 15:42:04 -0500

    wrote method to GET all content from database in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,35 +1,59 @@
 import { openDB } from 'idb';
 
+// initiate database named 'jate', version 1
 const initdb = async () =>
    openDB('jate', 1, {
+      // add database schema if it has not already been initialized
       upgrade(db) {
          if (db.objectStoreNames.contains('jate')) {
             console.log('jate database already exists');
             return;
          }
+         // create a new object store for data and give it key name of 'id', set to auto-increment
          db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
          console.log('jate database created');
       },
    });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
-   console.error('putDb not implemented');
 
-   // connect to database
+// method that accepts content and adds it to the database
+export const putDb = async (content) => {
+   // create connection to the IndexedDB database and the version we want to use
    const jateDb = await openDB('jate', 1);
 
+   // create a new transaction and specify the store/data priveleges
    const tx = jateDb.transaction('jate', 'readwrite');
 
+   // open the 'jate' object store
    const store = tx.objectStore('jate');
 
+   // use the .put() method to add new data to the database
    const request = store.put({ id: 1, value: content });
 
+   // get confirmation of the request
    const result = await request;
-      console.log('Text saved to database!', result.value);
+   console.log('Text saved to database!', result.value);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+
+// method that gets all content from the database
+export const getDb = async () => {
+   // create connection to the IndexedDB database and the version we want to use
+   const jateDb = await openDB('jate', 1);
+
+   // create a new transaction and specify the store/data priveleges
+   const tx = jateDb.transaction('jate', 'readonly');
+
+   // open the 'jate' object store
+   const store = tx.objectStore('jate');
+
+   // use the .get() method to retreive all data from the database
+   const request = store.get(1);
+
+   // get confirmation of the request
+   const result = await request;
+   console.log('result.value', result.value);
+   return result.value;
+};
 
 initdb();
